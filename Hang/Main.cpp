4ee6e8e2bc67f5fc4dfb351042eb5fa529c800b1
Keloran <keloran@nordicarts.net>
2014-11-03 20:34:01 +0000

    shit man

--- Hang/Main.cpp
+++ Hang/Main.cpp
@@ -2,7 +2,11 @@
 
 namespace NordicArts {
   int Main() {
-      printIt("Tester");
+      std::cout << "----" << std::endl;
+      std::cout << "|  o" << std::endl;
+      std::cout << "| -|-" << std::endl;
+      std::cout << "| / \\" << std::endl;
+      std::cout << "__" << std::endl;
 
       return EXIT_SUCCESS;
   }
