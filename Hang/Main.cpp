aed12f743c7cbbcabc8825e81a09bf39c945b0c5
Keloran <keloran@nordicarts.net>
2014-11-03 20:02:48 +0000

    started

---
+++ Hang/Main.cpp
@@ -0,0 +1,13 @@
+#include "Main.hpp"
+
+namespace NordicArts {
+  int Main() {
+      printIt("Tester");
+
+      return EXIT_SUCCESS;
+  }
+};
+
+int main() {
+    return NordicArts::Main();
+}
---
+++ Hang/Main.hpp
@@ -0,0 +1,33 @@
+#ifndef Main_H
+#define Main_H
+
+// Includes
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdexcept>
+#include <cstddef>
+#include <stdarg.h>
+#include <stdint.h>
+#include <iomanip>
+#include <math.h>
+#include <ios>
+
+// OSX Includes
+#ifdef __OBJC__
+    #import <Cocoa/Cocoa.h>
+    #import <Foundation/Foundation.h>
+    #import <CoreFoundation/CoreFoundation.h>
+#endif
+
+// Errors
+#ifndef printStuff
+    #define printStuff
+    #define printIt(variable) { std::cout << #variable << " " << variable << std::endl; }
+#endif
+
+namespace NordicArts {
+    int Main();
+};
+
+#endif
