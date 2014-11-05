b01fb103395ee6f9e12b7ab7e2c84b270ffeed0b
Keloran <keloran@develbox.info>
2014-11-05 12:06:05 +0000

    better looing dead man

--- Hang/DeadMan/DeadMan.cpp
+++ Hang/DeadMan/DeadMan.cpp
@@ -5,87 +5,96 @@ namespace NordicArts {
     }
 
     void DeadMan::life11() {
-        std::cout << "__" << std::endl;
+        std::cout << "___" << std::endl << std::endl;
     }
 
     void DeadMan::life10() {
         std::cout << "| " << std::endl;
-        std::cout << "__" << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;
     }
 
     void DeadMan::life9() {
         std::cout << "| " << std::endl;
         std::cout << "| " << std::endl;
-        std::cout << "__" << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;
     }
 
     void DeadMan::life8() {
         std::cout << "| " << std::endl;
         std::cout << "| " << std::endl;
         std::cout << "| " << std::endl;
-        std::cout << "__" << std::endl;
+        std::cout << "| " << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;
     }
 
     void DeadMan::life7() {
-        std::cout << "--" << std::endl;
+        std::cout << "___" << std::endl;
+        std::cout << "|/ " << std::endl;
         std::cout << "| " << std::endl;
         std::cout << "| " << std::endl;
         std::cout << "| " << std::endl;
-        std::cout << "__" << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;
     }
 
     void DeadMan::life6() {
-        std::cout << "----" << std::endl;
+        std::cout << "______" << std::endl;
+        std::cout << "|/ " << std::endl;
         std::cout << "| " << std::endl;
         std::cout << "| " << std::endl;
         std::cout << "| " << std::endl;
-        std::cout << "__" << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;
     }
 
     void DeadMan::life5() {
-        std::cout << "----" << std::endl;
-        std::cout << "|  o" << std::endl;
+        std::cout << "______" << std::endl;
+        std::cout << "|/   |" << std::endl;
+        std::cout << "|    o" << std::endl;
         std::cout << "| " << std::endl;
         std::cout << "| " << std::endl;
-        std::cout << "__" << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;
     }
 
     void DeadMan::life4() {
-        std::cout << "----" << std::endl;
-        std::cout << "|  o" << std::endl;
-        std::cout << "|  |" << std::endl;
+        std::cout << "______" << std::endl;
+        std::cout << "|/   |" << std::endl;
+        std::cout << "|    o" << std::endl;
+        std::cout << "|    |" << std::endl;
         std::cout << "| " << std::endl;
-        std::cout << "__" << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;;
     }
 
     void DeadMan::life3() {
-        std::cout << "----" << std::endl;
-        std::cout << "|  o" << std::endl;
-        std::cout << "| -|" << std::endl;
+        std::cout << "______" << std::endl;
+        std::cout << "|/   |" << std::endl;
+        std::cout << "|    o" << std::endl;
+        std::cout << "|   -|" << std::endl;
         std::cout << "| " << std::endl;
-        std::cout << "__" << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;
     }
 
     void DeadMan::life2() {
-        std::cout << "----" << std::endl;
-        std::cout << "|  o" << std::endl;
-        std::cout << "| -|-" << std::endl;
-        std::cout << "| " << std::endl;
-        std::cout << "__" << std::endl;
+        std::cout << "______" << std::endl;
+        std::cout << "|/  |" << std::endl;
+        std::cout << "|   o" << std::endl;
+        std::cout << "|  -|-" << std::endl;
+        std::cout << "|" << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;;
     }
-
     void DeadMan::life1() {
-        std::cout << "----" << std::endl;
-        std::cout << "|  o" << std::endl;
-        std::cout << "| -|-" << std::endl;
-        std::cout << "| /" << std::endl;
+        std::cout << "______" << std::endl;
+        std::cout << "|/   |" << std::endl;
+        std::cout << "|    o" << std::endl;
+        std::cout << "|   -|-" << std::endl;
+        std::cout << "|   /" << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;
     }
 
     void DeadMan::dead() {
-        std::cout << "----" << std::endl;
-        std::cout << "|  o" << std::endl;
-        std::cout << "| -|-" << std::endl;
-        std::cout << "| / \\" << std::endl;
-        std::cout << "__" << std::endl;
+        std::cout << "______" << std::endl;
+        std::cout << "|/   |" << std::endl;
+        std::cout << "|    o" << std::endl;
+        std::cout << "|   -|-" << std::endl;
+        std::cout << "|   / \\ " << std::endl;
+        std::cout << "|\\_" << std::endl << std::endl;
     }
 };
